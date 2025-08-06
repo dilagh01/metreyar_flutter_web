49392dcb96bae8116f8f13f5ef3943eab7b093aa
Dilagh <Dilagh01@gmail.com>
2025-08-06 14:07:06 +0000

    🚀 Initial commit via SSH

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Metreyar Flutter Web',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello from Web & Android'),
+        ),
+        body: const Center(
+          child: Text('Welcome to Metreyar!'),
+        ),
+      ),
+    );
+  }
+}
