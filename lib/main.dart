3495cb73688cc3f499628f9ce539c43eceb64305
Dilagh <Dilagh01@gmail.com>
2025-08-10 19:39:34 +0000

    🚀 Initial commit via SSH

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: OCRHighlightapage(), // صفحه اصلی OCRPage
+      home: OCRHighlightaPage(), // صفحه اصلی OCRPage
     );
   }
 }
