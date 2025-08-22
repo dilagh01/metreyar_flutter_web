056c2cac37c7336b7815aa1bc5c249ed175d97d5
Dilagh <Dilagh01@gmail.com>
2025-08-22 20:24:24 +0000

    🚀 Initial commit via SSH

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-// صفحه Features
-class FeaturesScreen extends StatelessWidget {
-  const FeaturesScreen({super.key}); // سازنده const
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Features')),
-      body: const Center(child: Text('Features Screen')),
-    );
-  }
-}
-
-// صفحه Home
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.go('/features'),
-          child: const Text('Go to Features'),
-        ),
-      ),
-    );
-  }
-}
+// Screens
+import 'screens/home_screen.dart';
+import 'screens/features_screen.dart';
+import 'screens/about_screen.dart';
 
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
   final GoRouter _router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => const HomeScreen(),
+        builder: (context, state) => HomeScreen(),
       ),
       GoRoute(
         path: '/features',
-        builder: (context, state) => const FeaturesScreen(),
+        builder: (context, state) => FeaturesScreen(),
+      ),
+      GoRoute(
+        path: '/about',
+        builder: (context, state) => AboutScreen(),
       ),
     ],
   );
-
-  runApp(MyApp(router: _router));
-}
-
-class MyApp extends StatelessWidget {
-  final GoRouter router;
-  const MyApp({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Metreyar Flutter Web',
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routerConfig: _router,
     );
   }
 }
