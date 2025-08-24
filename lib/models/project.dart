dad98aa2123e89bd03621b4e55e5ff17542f4ed9
Dilagh <Dilagh01@gmail.com>
2025-08-24 17:57:28 +0000

    🚀 Initial commit via SSH

---
+++ lib/models/project.dart
@@ -0,0 +1,7 @@
+
+class Project {
+  final String name;
+  final DateTime createdAt;
+
+  Project({required this.name, required this.createdAt});
+}
---
+++ lib/models/row_item.dart
@@ -0,0 +1,7 @@
+
+class RowItem {
+  final String title;
+  final String value;
+
+  RowItem({required this.title, required this.value});
+}
