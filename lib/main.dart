8267a28dd828dde0482687b943dab9e685a8156a
Dilagh <Dilagh01@gmail.com>
2025-08-25 18:41:22 +0000

    🚀 Initial commit via SSH

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// screens
+// Screens
 import 'screens/home_screen.dart';
 import 'screens/projects_screen.dart';
 import 'screens/contracts_screen.dart';
@@ -13,6 +13,19 @@ import 'screens/report_screen.dart';
 import 'screens/settings_screen.dart';
 import 'screens/workflow_screen.dart';
 
+// Widgets
+import 'widgets/sidebar.dart';
+import 'widgets/footer.dart';
+import 'widgets/header.dart';
+import 'widgets/nav_bar.dart';
+import 'widgets/responsive_scaffold.dart';
+import 'widgets/row_item.dart';
+import 'widgets/row_editor.dart';
+import 'widgets/data_table_widget.dart';
+import 'widgets/chart_widget.dart';
+import 'widgets/feature_card.dart';
+import 'widgets/new_widget.dart';
+
 void main() {
   runApp(MetreyarApp());
 }
@@ -21,11 +34,11 @@ class MetreyarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Metreyar',
       debugShowCheckedModeBanner: false,
+      title: "متره یار",
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
+        fontFamily: "Vazir",
       ),
       initialRoute: '/',
       routes: {
@@ -43,4 +56,4 @@ class MetreyarApp extends StatelessWidget {
       },
     );
   }
-}
+}		
