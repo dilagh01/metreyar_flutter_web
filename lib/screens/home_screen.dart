5be344b69b9eb6b45490933c7f8a27eaf76fb3da
Dilagh <Dilagh01@gmail.com>
2025-08-26 04:33:59 +0000

    🚀 Initial commit via SSH

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,21 @@
+// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
-import 'metreyar_table.dart';
+import '../widgets/metreyar_table.dart';
 import '../widgets/row_item.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
   final List<RowItem> tableRows = [
-    RowItem(index: 1, item: 'سیمان', unit: 'کیسه', quantity: 20, price: 2000000),
-    RowItem(index: 2, item: 'میلگرد', unit: 'کیلوگرم', quantity: 150, price: 15000000),
+    RowItem(col1: 'ردیف 1', col2: 'مقدار 1'),
+    RowItem(col1: 'ردیف 2', col2: 'مقدار 2'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: MetreyarTable(rows: tableRows),
-        ),
+        child: MetreyarTable(rows: tableRows),
       ),
     );
   }
