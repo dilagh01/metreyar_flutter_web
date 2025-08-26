76145acff49b9cdf8fcb08de3ba697f7747b5b6b
Dilagh <Dilagh01@gmail.com>
2025-08-26 04:49:35 +0000

    🚀 Initial commit via SSH

--- lib/widgets/row_item.dart
+++ lib/widgets/row_item.dart
@@ -1,6 +1,10 @@
 class RowItem {
   final String title;
   final dynamic value;
+  final String col1;
+  final String col2;
 
   RowItem({required this.title, required this.value});
+  RowItem({required this.col1, required this.col2});
 }
+
