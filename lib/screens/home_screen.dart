3fe0ea36a7311830b6783b4a85b055496343d658
Dilagh <Dilagh01@gmail.com>
2025-08-26 04:54:46 +0000

    🚀 Initial commit via SSH

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,24 @@
-// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
-import '../widgets/metreyar_table.dart';
 import '../widgets/row_item.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
-
-  final List<RowItem> tableRows = [
-    RowItem(col1: 'ردیف 1', col2: 'مقدار 1'),
-    RowItem(col1: 'ردیف 2', col2: 'مقدار 2'),
-  ];
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: MetreyarTable(rows: tableRows),
+      appBar: AppBar(
+        title: const Text('صفحه اصلی'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: const [
+            RowItem(col1: 'ردیف 1', col2: 'مقدار 1'),
+            RowItem(col1: 'ردیف 2', col2: 'مقدار 2'),
+            RowItem(col1: 'ردیف 3', col2: 'مقدار 3'),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/row_item.dart
+++ lib/widgets/row_item.dart
@@ -1,10 +1,28 @@
-class RowItem {
-  final String title;
-  final dynamic value;
+import 'package:flutter/material.dart';
+
+class RowItem extends StatelessWidget {
   final String col1;
   final String col2;
 
-  RowItem({required this.title, required this.value});
-  RowItem({required this.col1, required this.col2});
-}
+  const RowItem({Key? key, required this.col1, required this.col2}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            col1,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            col2,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
