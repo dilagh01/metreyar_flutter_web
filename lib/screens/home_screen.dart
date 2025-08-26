db6d9647b158e787c320954b0875ecd2ee8768f7
Dilagh <Dilagh01@gmail.com>
2025-08-26 04:01:27 +0000

    🚀 Initial commit via SSH

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import '../widgets/sidebar.dart';
+import '../widgets/responsive_scaffold.dart';
+import '../widgets/metreyar_table.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("خانه")),
-      drawer: SideBar(
-        onItemTap: (route) {
-          Navigator.pushNamed(context, route);
-        },
-      ),
-      body: Center(
-        child: Text("به متره یار خوش آمدید"),
+    return ResponsiveScaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text('داشبورد پروژه‌ها', style: Theme.of(context).textTheme.headline5),
+            SizedBox(height: 20),
+            MetreyarTable(), // جدول نمونه
+          ],
+        ),
       ),
     );
   }
