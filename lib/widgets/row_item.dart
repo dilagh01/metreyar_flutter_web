33f3f93f2029e76d249a229d9738c2e6172c809c
Dilagh <Dilagh01@gmail.com>
2025-08-26 04:39:47 +0000

    🚀 Initial commit via SSH

--- lib/widgets/row_item.dart
+++ lib/widgets/row_item.dart
@@ -1,15 +1,7 @@
 class RowItem {
-  final int index;
-  final String item;
-  final String unit;
-  final int quantity;
-  final int price;
+  final String col1;
+  final String col2;
 
-  RowItem({
-    required this.index,
-    required this.item,
-    required this.unit,
-    required this.quantity,
-    required this.price,
-  });
+  // حتما named parameters با required
+  RowItem({required this.col1, required this.col2});
 }
