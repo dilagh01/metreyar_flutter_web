582e42dad51cd6e063d41deac582ccafd7091035
Dilagh <Dilagh01@gmail.com>
2025-08-27 07:41:58 +0000

    🚀 Initial commit via SSH

--- lib/screens/estimation_screen.dart
+++ lib/screens/estimation_screen.dart
@@ -8,23 +8,30 @@ class EstimationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("متره و تدکار")),
+      appBar: AppBar(
+        title: const Text("متره و تدکار"),
+      ),
       drawer: SideBar(
         onItemTap: (route) {
           Navigator.pushNamed(context, route);
         },
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               "لیست اقلام متره",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
-            Expanded(child: SingleChildScrollView(child: EstimationTable())),
+            const SizedBox(height: 16),
+            // حذف const از اینجا تا StatefulWidget بتواند ساخته شود
+            Expanded(
+              child: SingleChildScrollView(
+                child: EstimationTable(),
+              ),
+            ),
           ],
         ),
       ),
