8bbaf3bfc163905993f92cc603c85a05ac7e63e8
Dilagh <Dilagh01@gmail.com>
2025-08-30 17:28:15 +0000

    🚀 Initial commit via SSH

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,24 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 // Screens
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/estimation_screen.dart';
-import 'screens/analysis_screen.dart';
 
-// Providers
-import 'providers/estimation_provider.dart';
-
-// Services
-import 'services/local_storage.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await LocalStorage().init();
-  
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => EstimationProvider()),
-      ],
-      child: MetreyarApp(),
-    ),
-  );
+void main() {
+  runApp(MetreyarApp());
 }
 
 class MetreyarApp extends StatelessWidget {
   MetreyarApp({super.key});
 
   final GoRouter _router = GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/', // تغییر به صفحه اصلی
     routes: [
-      GoRoute(path: '/login', builder: (context, state) => LoginScreen()),
+      GoRoute(path: '/', builder: (context, state) => LoginScreen()), // صفحه لاگین به عنوان صفحه اصلی
       GoRoute(path: '/home', builder: (context, state) => HomeScreen()),
       GoRoute(path: '/estimation', builder: (context, state) => EstimationScreen()),
-      GoRoute(path: '/analysis', builder: (context, state) => AnalysisScreen()),
     ],
   );
 
