a66f3b052f712e011997417418ba68325ed58aaf
Dilagh <Dilagh01@gmail.com>
2025-08-30 15:50:06 +0000

    🚀 Initial commit via SSH

--- lib/utils/estimation_calculator.dart
+++ lib/utils/estimation_calculator.dart
@@ -1,24 +1,15 @@
-// lib/utils/estimation_calculator.dart
 import '../models/row_item.dart';
 
 class EstimationCalculator {
   static double calculateTotal(List<RowItem> items) {
-    return items.fold(0, (sum, item) => sum + (item.quantity * item.unitPrice));
+    return items.fold(0, (sum, item) => sum + item.totalPrice);
   }
 
   static Map<String, double> calculateCategoryTotals(List<RowItem> items) {
     Map<String, double> totals = {};
     for (var item in items) {
-      totals[item.category] = (totals[item.category] ?? 0) + (item.quantity * item.unitPrice);
+      totals[item.category] = (totals[item.category] ?? 0) + item.totalPrice;
     }
     return totals;
-  }
-
-  static double calculateWithWaste(double quantity, double wastePercentage) {
-    return quantity * (1 + wastePercentage / 100);
-  }
-
-  static double calculateWithTax(double amount, double taxPercentage) {
-    return amount * (1 + taxPercentage / 100);
   }
 }
