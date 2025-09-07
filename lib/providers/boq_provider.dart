0c9480c1753169afea38e360943831b467ef1019
Dilagh <Dilagh01@gmail.com>
2025-09-07 15:16:03 +0000

    🚀 Initial commit via SSH

--- lib/providers/boq_provider.dart
+++ lib/providers/boq_provider.dart
@@ -1,25 +1,29 @@
-/material.dart';
+import 'package:flutter/material.dart';
 import '../models/boq_item.dart';
 
 class BOQProvider extends ChangeNotifier {
-  List<BOQItem> items = [];
+  List<BOQItem> _items = [];
 
-  double get totalCost =>
-      items.fold(0, (sum, item) => sum + item.totalPrice);
+  List<BOQItem> get items => _items;
 
   void addItem(BOQItem item) {
-    items.add(item);
+    _items.add(item);
     notifyListeners();
   }
 
-  void removeItem(int index) {
-    items.removeAt(index);
+  void removeItem(BOQItem item) {
+    _items.remove(item);
     notifyListeners();
   }
 
   void updateItem(int index, BOQItem newItem) {
-    items[index] = newItem;
-    items[index].recalc();
-    notifyListeners();
+    if (index >= 0 && index < _items.length) {
+      _items[index] = newItem;
+      notifyListeners();
+    }
+  }
+
+  double get totalPrice {
+    return _items.fold(0, (sum, item) => sum + item.totalPrice);
   }
 }
