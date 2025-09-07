f96571295dfc66e8041be4282d4f4e0850547c75
Dilagh <Dilagh01@gmail.com>
2025-09-07 10:13:21 +0000

    🚀 Initial commit via SSH

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// Import‌های placeholder (بعداً با مسیر واقعی پروژه جایگزین کن)
+import 'providers/estimation_provider.dart';
+import 'screens/materials_screen.dart';
+
 void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => EstimationProvider()),
       ],
-      child: MaterialApp(
-        home: MaterialsScreen(), // یا CalculationScreen
-      ),
+      child: const MetreyarApp(),
     ),
   );
+}
+
+class MetreyarApp extends StatelessWidget {
+  const MetreyarApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Metreyar Flutter Web',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MaterialsScreen(),
+    );
+  }
 }
