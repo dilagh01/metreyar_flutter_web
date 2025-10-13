46e7cad39be5219faf00e82023e625e2005bdc0a
dilagh01 <dilagh01@users.noreply.github.com>
2025-10-13 18:17:25 +0000

    deploy: 71657b3187b8173f1e43b262a645c9cff5cf23e4

---
+++ fluter.js
@@ -0,0 +1,3 @@
+// این نسخه باید از Flutter SDK کپی شود
+// مسیر پیش‌فرض: flutter/bin/cache/flutter_web_sdk/flutter.js
+// فقط فایل را در web/ پروژه قرار بده
---
+++ flutter.js
@@ -0,0 +1,4 @@
+// Placeholder flutter.js
+function flutterMain() {
+  console.log("Flutter Web bootstrap placeholder loaded");
+}
